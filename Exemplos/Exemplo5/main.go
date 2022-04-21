e963524310cebe86e29ef4b9a98a9becf4de0415
Douglas Oliveira <douglasol@msn.com>
2022-04-21 13:26:58 -0300

    maps

---
+++ Exemplos/Exemplo5/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var pessoas = make([]map[string]string, 0)
+	pessoas = append(pessoas, pessoaADD("Zé", "Mané"))
+	pessoas = append(pessoas, pessoaADD("Maria", "Silva"))
+	for _, pessoa := range pessoas {
+		fmt.Printf("%v, %v\n", pessoa["nome"], pessoa["sobrenome"])
+	}
+
+}
+
+func pessoaADD(nome string, sobrenome string) map[string]string {
+	var pessoa = make(map[string]string)
+	pessoa["nome"] = nome
+	pessoa["sobrenome"] = sobrenome
+	return pessoa
+}
