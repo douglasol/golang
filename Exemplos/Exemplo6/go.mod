9fe629b77ff51991ad5d55956199951f3d2050b1
Douglas Oliveira <douglasol@msn.com>
2022-04-21 13:51:56 -0300

    struct

---
+++ Exemplos/Exemplo6/go.mod
@@ -0,0 +1,3 @@
+module exemplo6
+
+go 1.18
---
+++ Exemplos/Exemplo6/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type Pessoa struct {
+	nome      string
+	sobrenome string
+	email     string
+	idade     uint
+}
+
+func main() {
+	var pessoas []Pessoa
+
+	pessoas = append(pessoas, pessoaADD("Zé", "Mané", "ze@ze.com", 10))
+	pessoas = append(pessoas, pessoaADD("Maria", "José", "zeze@zeze.com", 10))
+	for _, pessoa := range pessoas {
+		fmt.Printf("%v, %v, %v, %v\n", pessoa.nome, pessoa.sobrenome, pessoa.email, pessoa.idade)
+	}
+}
+
+func pessoaADD(nome string, sobrenome string, email string, idade uint) Pessoa {
+	/*
+		var pessoa Pessoa
+		pessoa.nome = nome
+		pessoa.sobrenome = sobrenome
+		pessoa.email = email
+		pessoa.idade = idade
+
+	*/
+	var pessoa = Pessoa{
+		nome:      nome,
+		sobrenome: sobrenome,
+		email:     email,
+		idade:     idade,
+	}
+	return pessoa
+
+}
