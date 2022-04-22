2db600b00e7868c1de2379b977d37d8fd6d802bf
Douglas Oliveira <douglasol@msn.com>
2022-04-22 13:29:15 -0300

    strings

--- Exemplos/Exemplo07/main.go
+++ Exemplos/Exemplo07/main.go
@@ -1,9 +1,14 @@
 package main
 
+/* string */
 import "fmt"
 
 func main() {
 	texto := "Isso é uma string"
 	fmt.Printf("%v %v %T", string(texto[2]), texto[2], texto[2])
 
+	/* incluir:
+	 	strings.TrimSpace()
+		[]byte to string
+	*/
 }
