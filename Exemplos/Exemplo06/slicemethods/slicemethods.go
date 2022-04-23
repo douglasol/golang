9d02e13bf1b608addc6b2a36748dc450c81f91a3
Douglas Oliveira <douglasol@msn.com>
2022-04-23 11:13:47 -0300

    metodos para slices

---
+++ Exemplos/Exemplo06/slicemethods/slicemethods.go
@@ -0,0 +1,37 @@
+package estruturadados
+
+/* insere um item na pilha */
+func PushItem(s []int, v int) []int {
+	return append(s, v)
+}
+
+/* insere um item na pilha */
+func PopItem(s []int) ([]int, int) {
+	v := s[len(s)-1]
+	return s[:len(s)-1], v
+}
+
+/* insere item a partir de um indice (melhor solução) */
+func InsertItemOnIndex(s []int, i int, v int) []int {
+	/* caso a posição seja maior que o tamanho do slice */
+	if i >= len(s) {
+		return append(s, v)
+	} else {
+		s = append(s[:i+1], s[i:]...)
+		s[i] = v
+		return s
+	}
+}
+
+/* remove tratando os indices  (melhor solução) */
+func RemoveItemOnIndex(s []int, i int) []int {
+	return append(s[:i], s[i+1:]...)
+}
+
+/* remove um item da coleção */
+func ReplaceItem(s []int, i int, v int) []int {
+	if i < len(s) {
+		s[i] = v
+	}
+	return s
+}
---
+++ Exemplos/Exemplo06/slicemethodsteste.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"exemplo6/slicemethods"
+	"fmt"
+)
+
+func main() {
+
+	/* array */
+	a := [4]int{10, 20, 30, 40}
+	fmt.Printf("array:%v\n", a)
+
+	/* slice */
+	s := []int{10, 20, 30, 40}
+	fmt.Printf("slice:%v\n", s)
+
+	/* append in slice */
+	s = append(s, 50)
+	s = append(s, 60)
+	s = append(s, 70)
+	s = append(s, 80)
+	s = append(s, 90)
+	s = append(s, 100)
+	fmt.Printf("append:%v\n", s)
+
+	/* push a item in stack */
+	fmt.Printf("remove, insert, replace\n")
+
+	/* remove item da coleção */
+	i := 2
+	fmt.Printf("remove: s=%v, i=%v, s[i]=%v\n", s, i, s[i])
+	s = slicemethods.RemoveItemOnIndex(s, i)
+	fmt.Printf("remove(index):%v\n", s)
+
+	/* insere um novo na posição */
+	v := 200 // valor a inserir
+	fmt.Printf("insert: s=%v, i=%v, s[i]=%v, v=%v\n", s, i, s[i], v)
+	s = slicemethods.InsertItemOnIndex(s, i, v)
+	fmt.Printf("insert:%v\n", s)
+
+	/* replace */
+	i = 2   // posicao a trocar
+	v = 300 // valor a trocar
+	fmt.Printf("replace: s=%v, i=%v, s[i]=%v, v=%v\n", s, i, s[i], v)
+	s = slicemethods.ReplaceItem(s, i, v)
+	fmt.Printf("replace:%v\n", s)
+
+	/* push and pop a item in stack */
+	fmt.Printf("pilha\n")
+
+	//Push(Object)	Inserts an object at the top of the Stack.
+	v = 400 // valor a empilhar
+	fmt.Printf("push:stack=%v, v=%v\n", s, v)
+	s = slicemethods.PushItem(s, v)
+	fmt.Printf("push:stack=%v\n", s)
+
+	//Pop()			Removes and returns the object at the top of the Stack.
+	fmt.Printf("pop:stack=%v\n", s)
+	s, v = slicemethods.PopItem(s)
+	fmt.Printf("pop:stack=%v, v=%v\n", s, v)
+
+	fmt.Printf("pop:stack=%v\n", s)
+	s, v = slicemethods.PopItem(s)
+	fmt.Printf("pop:stack=%v, v=%v\n", s, v)
+
+	fmt.Printf("pop:stack=%v\n", s)
+	s, v = slicemethods.PopItem(s)
+	fmt.Printf("pop:stack=%v, v=%v\n", s, v)
+}
